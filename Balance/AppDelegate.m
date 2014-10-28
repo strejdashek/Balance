e9341925042f353d993295ba13477d357adc22fd
Viktor Kucera <vikousek@Brno-macbook.local>
2014-10-28 13:05:58 +0100

    comment seed

--- Balance/AppDelegate.m
+++ Balance/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    //seed test data, uncomment if needed
     //[CoreDataManager seed];
     
     return YES;
